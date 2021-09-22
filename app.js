6d59d4a70f635af59eff388bc8a7aef0af27e68b
juannetto <jnetto60@gmail.com>
2021-09-21 21:34:39 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+app.use(express.static('./public'));
+
+app.listen(3000, ()=>{
+console.log('servidor funcionando');
+});
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/home', (req, res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/compare', (req, res)=>{
+    res.sendFile(__dirname + '/views/compare.html');
+});
+
+app.get('/login', (req, res) =>{
+    res.sendFile(__dirname + '/views/login.html');
+})
+
+app.get('/product', (req, res) =>{
+    res.sendFile(__dirname + '/views/Product.html');
+})
+
+app.get('/cart', (req, res) =>{
+    res.sendFile(__dirname + '/views/cart.html');
+})
